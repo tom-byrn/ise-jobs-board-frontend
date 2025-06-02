8c97cdc1b1dac604053a7efd1da54915efced36c
Bence R <bencethegreat@gmail.com>
2025-06-02 18:55:37 +0100

    Changed hihglight colour wow

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { ThemeProvider } from "@/components/theming/theme-provider";
 import { Navbar } from "@/components/navbar/navbar";
-import { createClient } from "@/lib/server";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +30,9 @@ export default async function MainLayout({
         className={`
             ${geistSans.variable} 
             ${geistMono.variable} 
-            antialiased font-mono [background-image:radial-gradient(circle_at_1px_1px,rgba(0,0,0,0.6)_1px,transparent_0)] [background-size:30px_30px] dark:[background-image:radial-gradient(circle_at_1px_1px,rgba(255,255,255,0.3)_1px,transparent_0)]`}
+            antialiased font-mono [background-image:radial-gradient(circle_at_1px_1px,rgba(0,0,0,0.6)_1px,transparent_0)] [background-size:30px_30px] dark:[background-image:radial-gradient(circle_at_1px_1px,rgba(255,255,255,0.3)_1px,transparent_0)]
+            selection:bg-black selection:text-white dark:selection:bg-white dark:selection:text-black
+`}
       >
         <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
           <Navbar />
