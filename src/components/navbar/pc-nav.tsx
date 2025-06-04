9f0f9394d53ececb0b1a449e86d30144d029c719
Bence R <bencethegreat@gmail.com>
2025-06-04 22:47:48 +0100

    Added notification at thetop of the screen

--- src/components/navbar/pc-nav.tsx
+++ src/components/navbar/pc-nav.tsx
@@ -19,33 +19,40 @@ import { DashboardLink } from "./dashboard-link";
 
 export function PcNavbar() {
   return (
-    <div className="max-w-screen bg-white dark:bg-black fixed z-50 hidden w-screen flex-row items-center border-b-2 border-neutral-900/20 p-3 font-mono tracking-tight backdrop-blur-sm dark:border-neutral-100/10 md:flex">
-      <Link href="/">
-        <Image
-          src={ISE_UL_LOGO}
-          width={200}
-          alt="The ISE and University of Limerick logos, side-by-side."
-          className="mr-6 invert dark:invert-0" />
-      </Link>
-      <NavigationMenu className="">
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="/job-postings" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Job Postings
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+    <div className="max-w-screen fixed z-50 hidden w-screen flex-col md:flex">
+      <div className="flex py-1 flex-row items-center bg-gradient-to-r from-green-400 to-green-600 text-center text-white">
+        <div className="mx-auto">
+          <span className="font-bold text-green-800">Upcoming Deadline!</span> Submit your R1 rankings by June 6th.
+        </div>
+      </div>
+      <div className="w-screen flex-row items-center border-b-2 border-neutral-900/20 bg-white p-2 font-mono tracking-tight dark:border-neutral-100/10 dark:bg-black md:flex">
+        <Link href="/">
+          <Image
+            src={ISE_UL_LOGO}
+            width={200}
+            alt="The ISE and University of Limerick logos, side-by-side."
+            className="mr-6 invert dark:invert-0" />
+        </Link>
+        <NavigationMenu className="">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/job-postings" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Job Postings
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
 
-          <DashboardLink />
+            <DashboardLink />
 
-        </NavigationMenuList>
-      </NavigationMenu>
+          </NavigationMenuList>
+        </NavigationMenu>
 
 
-      <div className="ml-auto mr-4 flex items-center" suppressHydrationWarning>
-        <ThemeSwapButton />
-        <LoginLogoutButton />
+        <div className="ml-auto mr-4 flex items-center" suppressHydrationWarning>
+          <ThemeSwapButton />
+          <LoginLogoutButton />
+        </div>
       </div>
     </div>
   );
